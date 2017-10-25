31e42efc6ee0689b3e8e8a0cbf463e5d3468a502
Frison B Alexander <fri.b.alex@gmail.com>
2017-10-25 17:57:35 -0400

    just barely getting started...

---
+++ Tensorflow/MNIST/notes.py
@@ -0,0 +1,13 @@
+#import tensorflow as tf
+#options for running python program in VS Code:
+    #1) [easy] Command Pallate (Ctrl+Shift+P) => Run Python File in Terminal
+    #[Others] 
+
+print("Starting program..")
+
+
+def pythonfunction():
+    print("this will be printed when the function is invoked")
+
+pythonfunction()
+
